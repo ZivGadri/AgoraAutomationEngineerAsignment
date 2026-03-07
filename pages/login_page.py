24c6d9808fe83fc4b445df6d9903f20b2b468f74
Ziv Gadri <ziv.automation@gmail.com>
2026-03-07 20:26:40 +0200

    Updating login_page.py with the elements and its needed functions

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,11 +1,35 @@
-"""
-pages/login_page.py
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from pages.base_page import BasePage, FindBy
+from pages.inventory_page import InventoryPage
+from pages.base_page import BasePage, FindBy
 
-Page Object for the SauceDemo login screen (https://www.saucedemo.com/).
-Handles all interactions and locators related to the login form.
+class LoginPage(BasePage):
+    """Page Object for the SauceDemo Login Page."""
+    
+    @FindBy(By.ID, "user-name")
+    def username_input(self) -> WebElement: pass
 
-Responsibilities:
-  - Entering username and password
-  - Submitting the login form
-  - Verifying successful or failed login state
-"""
+    @FindBy(By.ID, "password")
+    def password_input(self) -> WebElement: pass
+
+    @FindBy(By.ID, "login-button")
+    def login_btn(self) -> WebElement: pass
+    
+    def fill_username(self, username: str) -> None:
+        self.type_text(self.username_input, username)
+        
+    def fill_password(self, password: str) -> None:
+        self.type_text(self.password_input, password)
+        
+    def click_login(self) -> InventoryPage:
+        self.click(self.login_btn)
+        return InventoryPage(self.driver)
+        
+    def login(self, username: str, password: str) -> InventoryPage:
+        """Helper method to perform a full login action and return the Inventory Page."""
+        self.fill_username(username)
+        self.fill_password(password)
+        inventory_page = self.click_login()
+        
+        return inventory_page
