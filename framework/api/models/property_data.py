e081246062d2324374515e7bde9d9cebdc7fe759
Ziv Gadri <ziv.automation@gmail.com>
2026-03-08 10:32:31 +0200

    Updating api module

--- framework/api/models/property_data.py
+++
@@ -1,6 +0,0 @@
-"""
-framework/api/models/property_data.py
-
-models package files contain the object models the application APIs work with.
-They provide an easy way for retrieving, holding and extracting the data on runtime
-"""
