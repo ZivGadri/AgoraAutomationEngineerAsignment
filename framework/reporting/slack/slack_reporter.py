00150c3f9b2c1ec1a0e112f0c322736d48c53308
Ziv Gadri <ziv.automation@gmail.com>
2026-03-08 10:13:15 +0200

    adding report module optional files - Allure and Slack

---
+++ framework/reporting/slack/slack_reporter.py
@@ -0,0 +1,12 @@
+"""
+framework/reporting/slack_reporter.py
+
+Optional integration with the Slack platform.
+All Slack webhooks are found here:
+https://hooks.slack.com/services/...
+
+When disabled, this module is a no-op and will not affect test results.
+
+Responsibilities:
+  - Sending test runs results in real time
+"""
